220a873baa37e692cb2356858f51aa49434d15b2
Naod Alemu <naodalemu11@gmail.com>
2022-03-13 01:37:42 +0300

    made everything work and added analog clock which was a pain in the a** figuring out the trignometric math

--- clock.js
+++ clock.js
@@ -7,15 +7,25 @@ let variable_6 = document.getElementById("second-hand");
 
 function updateClock() {
   let hour_store = new Date().getHours();
-  let minuet_store = new Date().getMinutes();
+  let minute_store = new Date().getMinutes();
   let second_store = new Date().getSeconds();
-  let hour_hand_store = new Date().getHours();
-  let minute_hand_store = new Date().getHours();
+  let hour_hand_store =
+    minute_store != 0 ? hour_store * 30 + minute_store * 0.5 : hour_store * 30;
+  let minute_hand_store =
+    second_store != 0
+      ? minute_store * 6 + second_store * 0.1
+      : minute_store * 6;
   let second_hand_store = second_store * 6;
 
   variable.innerHTML = hour_store;
-  variable_2.innerHTML = minuet_store;
+  variable_2.innerHTML = minute_store;
   variable_3.innerHTML = second_store;
+  variable_4.style.transform =
+    "rotate(" + hour_hand_store + "deg) translateY(-45px)";
+  variable_5.style.transform =
+    "rotate(" + minute_hand_store + "deg) translateY(-45px)";
+  variable_6.style.transform =
+    "rotate(" + second_hand_store + "deg) translateY(-45px)";
 }
-
-setInterval(updateClock, 10);
+updateClock();
+setInterval(updateClock, 1000);
